9403286fce77f6c95eddca28de77fc07fc929e68
Gugucha <gugucha.g@gmail.com>
2022-10-06 13:30:20 +0400

    asd

---
+++ app.js
@@ -0,0 +1,14 @@
+﻿const b = document.querySelector('button.button--NO');
+b.addEventListener("click", mouseHover)
+function mouseHover() {
+    const i = Math.floor(Math.random() * 500) + 1;
+    const j = Math.floor(Math.random() * 500) + 1;
+    b.style.left = i + "px"
+    b.style.top=j+"px"
+}
+const c = document.querySelector('button.button--YES')
+c.addEventListener("click", mouseclick)
+function mouseclick() {
+    confirm("I Love You Too ❤️");
+
+}
